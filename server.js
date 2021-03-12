e0f619d70f8576768ac99073e5056a1d3e0b1771
Rushikesh Bosamia <rushikeshbosamiya95@gmail.com>
2021-03-12 10:00:06 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(cors({
   credentials:true
 }));
 app.use(morgan('common'));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.use('/',homeRouter);
